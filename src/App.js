f4a32bccebfa292d92cba038374b97b79b2b3db2
SudharshanBhimisetty <sudharshanbhimisetty24@gmail.com>
2021-01-28 20:03:54 +0530

    build is changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useEffect, useState} from 'react'
 import SingleColor from './SingleColor';
-// import hexToRgb from './hexToRgb.js';
+
 import Values from 'values.js';
 
 
@@ -9,18 +9,6 @@ function App() {
   const [color, setcolor] = useState('')
   const [error, seterror] = useState(false)
   const [list, setlist] = useState([])
-
-
-/* const conversion = () => {
- const rgbColor =  hexToRgb(color)
- if(rgbColor != null){
-   console.log(rgbColor.r + "," + rgbColor.g + "," + rgbColor.b)
-   return rgbColor.r + "," + rgbColor.g + "," + rgbColor.b
- }else{
-   return ""
- }
-
-} */
 
 useEffect(()=>{
   try{
@@ -31,22 +19,15 @@ useEffect(()=>{
       seterror(false)
     }
 else{
-  // seterror(true)
   setlist([])
 }
   }catch(error){
-    // seterror(true)
     setlist([])
   }
-
 },[color])
-
 const handleInput = async(e) =>{
   setcolor(e.target.value)
 }
-
-
-
 
   return (
     <div className="App">
