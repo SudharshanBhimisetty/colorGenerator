c66cafba10b9fc9d39715ea2542f6e982c481195
SudharshanBhimisetty <sudharshanbhimisetty24@gmail.com>
2021-01-28 22:20:27 +0530

    rgb conversion is added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React,{useEffect, useState} from 'react'
 import SingleColor from './SingleColor';
-
+import rgbToHex from './rgbToHex';
 import Values from 'values.js';
 
 
 function App() {
 
   const [color, setcolor] = useState('')
-  const [error, seterror] = useState(false)
   const [list, setlist] = useState([])
-
+  const [rgb, setrgb] = useState("")
+ 
 useEffect(()=>{
   try{
     let colors = new Values(color).all(10)
     if(colors[1].alpha == 1){
       console.log(colors)
       setlist(colors)
-      seterror(false)
+      console.log(colors[10].rgb)
+      setrgb(colors[10].rgb)
     }
 else{
   setlist([])
@@ -25,13 +26,36 @@ else{
     setlist([])
   }
 },[color])
+
+
 const handleInput = async(e) =>{
   setcolor(e.target.value)
 }
 
+const converter = (e) => {
+  e.preventDefault()
+  const rgbArray = rgb.split(',')
+  const pass = rgbToHex(parseInt(rgbArray[0]),parseInt(rgbArray[1]),parseInt(rgbArray[2]))
+ setcolor(pass)
+}
+
+
+
   return (
     <div className="App">
+    <div className="converter-box">
+    <div className="converter">
+              <h3>Rgb to hex converter</h3>
+              <form onSubmit={converter}>
+                 <input type="text" placeholder="255,255,255" value={rgb} onChange={(e) => setrgb(e.target.value)}></input>
+                 <button className="btn">Convert</button>
+              </form>
+             
+          </div>
+    </div>
+
           <h3 className="heading">Color generator</h3>
+
           <div className="forms">
               <form>
                        <label>Hex: </label>
---
+++ src/rgbToHex.js
@@ -0,0 +1,20 @@
+/* function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
+  
+  function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  }
+   */
+
+
+  const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
+    const hex = x.toString(16)
+    return hex.length === 1 ? '0' + hex : hex
+  }).join('')
+  
+//   console.log(rgbToHex(0, 51, 255)); 
+
+
+  export default rgbToHex;
